903d7629a92baa1dbf76ce39036ddc48dfb68cf0
Brandon Williams <mbrandonw@hey.com>
2026-01-20 12:41:58 -0600

    wip

--- Sources/pfw-cli/Main.swift
+++ Sources/pfw-cli/Main.swift
@@ -7,11 +7,13 @@ struct PFW: AsyncParsableCommand {
   static let configuration = CommandConfiguration(
     commandName: "pfw",
     abstract: "CLI for managing Point-Free Way skills.",
+    version: PFWVersion.current,
     subcommands: [
       Login.self,
       Logout.self,
       Status.self,
       Install.self,
+      Version.self,
     ]
   )
 }
