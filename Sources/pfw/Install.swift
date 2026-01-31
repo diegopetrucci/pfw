32eb2faafbcfb5b237afdd4c4ee095355144db26
Stephen Celis <stephen@stephencelis.com>
2026-01-30 17:24:59 -0800

    Add pfw version check (#22)
    * wip
    
    * wip

--- Sources/pfw/Install.swift
+++ Sources/pfw/Install.swift
@@ -79,7 +79,24 @@ struct Install: AsyncParsableCommand {
   var force = false
 
   func run() async throws {
+    @Dependency(\.gitHub) var gitHub
+
     try await install(shouldRetryAfterLogin: true)
+    do {
+      let tags = try await gitHub.fetchTags(owner: "pointfreeco", repo: "homebrew-tap")
+      let mostRecentTag = tags
+        .first { $0.name.hasPrefix("pfw-") }
+        .map { String($0.name.dropFirst(4)) }
+      if let mostRecentTag, mostRecentTag != PFW.configuration.version {
+        print(
+          """
+          
+          pfw \(mostRecentTag) is available. Run 'brew update && brew upgrade pfw' to install.
+          """
+        )
+      }
+    } catch {
+    }
   }
 
   private func install(shouldRetryAfterLogin: Bool) async throws {
